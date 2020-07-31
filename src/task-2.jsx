02b46363062e721278bf47199b7ef854bc2dbea5
Serhiy Saranchuk <serhiyzsd@gmail.com>
2020-07-31 16:51:55 +0300

    fixes

--- src/task-2.jsx
+++ src/task-2.jsx
@@ -13,12 +13,12 @@ class Calculator extends React.Component {
     this.handleChangeOperand2 = this.handleChangeOperand2.bind(this);
     this.handleClick = this.handleClick.bind(this)
   }
-  handleChangeOperand1(event) {
-    this.setState({operand1: event.target.value});
+  handleChangeOperand1(e) {
+    this.setState({operand1: e.target.value});
   }
 
-  handleChangeOperand2(event) {
-    this.setState({operand2: event.target.value});
+  handleChangeOperand2(e) {
+    this.setState({operand2: e.target.value});
   }
 
   handleClick(event) {
@@ -26,18 +26,21 @@ class Calculator extends React.Component {
     let result;
     switch(operationType) {
     case 'Add': result = Number(this.state.operand1) + Number(this.state.operand2);
+      this.setState({result: result});
       break;
     case 'Subtract' : result = Number(this.state.operand1) - Number(this.state.operand2);
+      this.setState({result: result});
       break;
-    case 'Multiple' : result = Number(this.state.operand1) * Number(this.state.operand2);
+    case 'Multiply' : result = Number(this.state.operand1) * Number(this.state.operand2);
+      this.setState({result: result});
       break;
     case 'Divide' : result = Number(this.state.operand1) / Number(this.state.operand2);
+      this.setState({result: result});
       break;
-    case 'Clear' : this.setState({operand1: 0, operand2: 0})
+    case 'Clear' : this.setState({operand1: 0, operand2: 0, result: 0})
       break;
     default:break;
     }
-    this.setState({result: result});
   }
   render() {
     return (
@@ -46,22 +49,22 @@ class Calculator extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-3">
-            <input type="number" value={this.state.operand1} onChange={this.handleChangeOperand1} placeholder="Operand 1"></input>
+            <input type="number"  value={this.state.operand1} className="form-control" onChange={this.handleChangeOperand1} placeholder="Operand 1"></input>
           </div>
           <div className="col-3">
-            <input type="number" value={this.state.operand2} onChange={this.handleChangeOperand2} placeholder="Operand 2"></input>
+            <input type="number" value={this.state.operand2} className="form-control" onChange={this.handleChangeOperand2} placeholder="Operand 2"></input>
           </div>
           <div className="col-2"><button className="btn btn-block btn-danger" onClick={this.handleClick}>Clear</button></div>
         </div>
         <div className="row my-3">
           <div className="col-2"><button className="btn btn-block btn-secondary" onClick={this.handleClick}>Add</button></div>
           <div className="col-2"><button className="btn btn-block btn-secondary" onClick={this.handleClick}>Subtract</button></div>
-          <div className="col-2"><button className="btn btn-block btn-secondary" onClick={this.handleClick}>Multiple</button></div>
+          <div className="col-2"><button className="btn btn-block btn-secondary" onClick={this.handleClick}>Multiply</button></div>
           <div className="col-2"><button className="btn btn-block btn-secondary" onClick={this.handleClick}>Divide</button></div>
         </div>
         <div className="row">
           <div className="col-4">
-            <input type='number' readOnly={true} value={this.state.result}/>
+            <input type='number' readOnly={true} className="form-control" value={this.state.result} placeholder="Result"/>
           </div>
         </div>
       </div>
